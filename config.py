d5478021620c94e963992de43d33273a704b5b4b
Kireet Agrawal <kireet@gmail.com>
2016-11-11 20:27:09 -0800

    Base Login System

---
+++ config.py
@@ -0,0 +1,10 @@
+WTF_CRSF_ENABLE = True
+SECRET_KEY = 'impossible-to-know-pword'
+
+
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+SQLALCHEMY_TRACK_MODIFICATIONS = True
