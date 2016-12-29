5155139d3878edf4b683c38f5aca8968cf5b2c7c
Kireet Agrawal <kireet@gmail.com>
2016-12-28 22:21:59 -0500

    deploy bug

--- app/forms.py
+++ app/forms.py
@@ -1,4 +1,4 @@
-from flask.ext.wtf import FlaskForm
+from flask_wtf import FlaskForm
 from wtforms import StringField, BooleanField, TextField, TextAreaField, SubmitField, validators, ValidationError, PasswordField
 from wtforms.validators import DataRequired, Optional
 from .models import User, Goal
